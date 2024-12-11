6dce0f5851d385b30169102c5571f0fdcff681b4
noahdotpy <noahdotpy@proton.me>
2024-12-11 18:29:06 +1000

    fix: apply system all now also runs block_flatpaks as it sohld

--- src/commands/apply/all.rs
+++ src/commands/apply/all.rs
@@ -9,5 +9,6 @@ pub fn apply_all() -> Result<()> {
     }
     crate::commands::apply::block_networking::apply_block_networking()?;
     crate::commands::apply::revoke_admin::apply_revoke_admin()?;
+    crate::commands::apply::block_flatpaks::apply_block_flatpaks()?;
     Ok(())
 }
